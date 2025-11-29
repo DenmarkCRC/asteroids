dad3a33c6701e8e973ca444abb591de22dd7c92d
DenmarkCRC <dennis@denmarkcrc.com.au>
2025-11-29 08:48:28 +0800

    boot.dev ch3l5 pass

--- main.py
+++ main.py
@@ -7,18 +7,27 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}") 
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT /2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         print(dt)
