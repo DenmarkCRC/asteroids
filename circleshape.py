805cf4ee630b0ba54067bb2482489ce02acca652
DenmarkCRC <dennis@denmarkcrc.com.au>
2025-11-30 10:20:39 +0800

    ch4l2 went good

--- circleshape.py
+++ circleshape.py
@@ -13,6 +13,12 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
+    def collides_with(self, other):
+        distance_between_centers = self.position.distance_to(other.position)
+        colision_distance = self.radius + other.radius
+        if distance_between_centers <= colision_distance:
+            return True
+
     def draw(self, screen):
         # must override
         pass
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}") 
@@ -13,11 +16,16 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
 
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT /2)
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
@@ -26,11 +34,16 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        print(dt)
+        #print(dt)
 
 if __name__ == "__main__":
     main()
